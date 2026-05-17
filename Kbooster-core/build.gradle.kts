20102660698eba5e6a9874a2ecf8870e234e4b93
badr qaba <qababadr@gmail.com>
2026-05-17 17:29:25 +0100

    using processor as pure jvm as well as core

--- Kbooster-core/build.gradle.kts
+++ Kbooster-core/build.gradle.kts
@@ -1,34 +1,10 @@
 plugins {
-    alias(libs.plugins.android.library)
+    kotlin("jvm")
     id("maven-publish")
 }
 
 kotlin {
     jvmToolchain(17)
-}
-
-android {
-    namespace = "com.badrqaba.kbooster.core"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    publishing {
-        singleVariant("release")
-    }
 }
 
 dependencies {
@@ -38,16 +14,14 @@ dependencies {
     compileOnly("com.google.android:android:4.1.1.4")
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                from(components["release"])
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
 
-                groupId = "com.github.badrqaba"
-                artifactId = "Kbooster-core"
-                version = project.version.toString()
-            }
+            groupId = "com.github.badrqaba"
+            artifactId = "Kbooster-core"
+            version = project.version.toString()
         }
     }
 }
--- Kbooster-processor/build.gradle.kts
+++ Kbooster-processor/build.gradle.kts
@@ -1,34 +1,10 @@
 plugins {
-    alias(libs.plugins.android.library)
+    kotlin("jvm")
     id("maven-publish")
 }
 
 kotlin {
     jvmToolchain(17)
-}
-
-android {
-    namespace = "com.badrqaba.kbooster.processor"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    publishing {
-        singleVariant("release")
-    }
 }
 
 dependencies {
@@ -39,20 +15,20 @@ dependencies {
     implementation("com.squareup:kotlinpoet:1.18.1")
     implementation("com.squareup:kotlinpoet-ksp:1.18.1")
 
+    // This allows you to reference Android classes (like Context or View)
+    // in the processor without making the processor an Android module.
     compileOnly("com.google.android:android:4.1.1.4")
 }
 
+// Standard Maven Publishing for JVM
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                from(components["release"])
-
-                groupId = "com.github.badrqaba"
-                artifactId = "Kbooster-processor"
-                version = project.version.toString()
-            }
+            groupId = "com.github.badrqaba"
+            artifactId = "Kbooster-processor"
+            version = project.version.toString()
         }
     }
 }
